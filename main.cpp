de423849885930251945eadf4fa2c73e6f24ad3a
Mateusz Salach <57152223+Salach-Malejkum@users.noreply.github.com>
2025-01-20 19:28:51 +0100

    Add parallel computing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <vector>
 #include <math.h>
+#include <thread>
+#include <chrono>
+
 #include "src/sphere/sphere.h"
 #include "src/light/light.h"
+
 
 const int C_WIDTH = 600, C_HEIGHT = 600, V_WIDTH = 1, V_HEIGHT = 1, d = 1;
 
@@ -162,6 +166,20 @@ void CreateLights(std::vector<std::unique_ptr<ILight>>& lights) {
 }
 
 
+void ParallelRayTracing(sf::Image& canvas, const std::vector<std::unique_ptr<ILight>>& lights, const std::vector<Sphere>& spheres, const Vector3D& origin, int width_min, int width_max, int heigth_min, int height_max, int rec_depth) {
+    for (int x = width_min; x < width_max; x++) 
+    {
+        for (int y = heigth_min; y < height_max; y++) 
+        {
+            Vector3D direction = CanvasToViewport(x, y);
+            sf::Color color = TraceRay(lights, spheres, origin, direction, 1, INFINITY, rec_depth);
+            sf::Vector2u pixel_pos(x + C_WIDTH / 2, y + C_HEIGHT / 2);
+            canvas.setPixel(pixel_pos, color);
+        }
+    }
+}
+
+
 int main()
 {
     Vector3D origin(0, 0, 0);
@@ -175,19 +193,23 @@ int main()
     sf::Image canvas;
     sf::Vector2u canvas_size(C_WIDTH, C_HEIGHT);
     canvas.resize(canvas_size, sf::Color::White);
-    int rec_depth = 3;
+    int rec_depth = 1;
 
-    for (int x = -C_WIDTH / 2; x < C_WIDTH / 2; x++) // 
-    {
-        for (int y = -C_HEIGHT / 2; y < C_HEIGHT / 2; y++) // 
-        {
-            
-            Vector3D direction = CanvasToViewport(x, y);
-            sf::Color color = TraceRay(lights, spheres, origin, direction, 1, INFINITY, rec_depth);
-            sf::Vector2u pixel_pos(x + C_WIDTH / 2, y + C_HEIGHT / 2);
-            canvas.setPixel(pixel_pos, color);
-        }
-    }
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::thread top_left(ParallelRayTracing, std::ref(canvas), std::ref(lights), std::ref(spheres), std::ref(origin), -C_WIDTH / 2, 0, -C_HEIGHT / 2, 0, rec_depth);
+    std::thread top_right(ParallelRayTracing, std::ref(canvas), std::ref(lights), std::ref(spheres), std::ref(origin), 0, C_WIDTH / 2, -C_HEIGHT / 2, 0, rec_depth);
+    std::thread bot_left(ParallelRayTracing, std::ref(canvas), std::ref(lights), std::ref(spheres), std::ref(origin), -C_WIDTH / 2, 0, 0, C_HEIGHT / 2, rec_depth);
+    std::thread bot_right(ParallelRayTracing, std::ref(canvas), std::ref(lights), std::ref(spheres), std::ref(origin), 0, C_WIDTH / 2, 0, C_HEIGHT / 2, rec_depth);
+
+    top_left.join();
+    top_right.join();
+    bot_left.join();
+    bot_right.join();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    printf("Execution time: %f", duration.count());
 
     const sf::Texture texture(canvas);
     sf::Sprite sprite(texture);
